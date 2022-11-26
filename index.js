3814df839776f8f22cbb1c454b0037304db5b4b1
Michelle-Mungai <chellymungai@gmail.com>
2022-11-26 19:35:38 +0300

    scope assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "jane"
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "sylvia"
+}
